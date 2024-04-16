9af6fcf4a11ed0c4705e89c615768e310895656a
Dhinesh Kumar <dhineshk6@gmail.com>
2024-04-15 22:15:37 -0400

    Update Compare.py

--- Compare.py
+++ Compare.py
@@ -19,31 +19,22 @@ def compare_log_files():
     file2_path = "file2.log"  # Hardcoded file path for file 2
     output_file = "comparison_output.txt"  # Output file path
 
-    with open(output_file, 'w') as output:
+    with open(file1_path, 'r') as file1, open(file2_path, 'r') as file2, open(output_file, 'w') as output:
         file1_data = read_log_file(file1_path)
         file2_data = read_log_file(file2_path)
 
         common_ids = set(file1_data.keys()) & set(file2_data.keys())
 
-        for scheduled_id in common_ids:
-            if file1_data[scheduled_id] != file2_data[scheduled_id]:
+        mismatched_ids = [scheduled_id for scheduled_id in common_ids if file1_data[scheduled_id] != file2_data[scheduled_id]]
+
+        if mismatched_ids:
+            output.write("Mismatched scheduled IDs:\n")
+            for scheduled_id in mismatched_ids:
                 output.write(f"Scheduled ID {scheduled_id} has different values in the two log files:\n")
                 output.write(f"File 1: {file1_data[scheduled_id]}\n")
                 output.write(f"File 2: {file2_data[scheduled_id]}\n\n")
-
-        unique_ids_file1 = set(file1_data.keys()) - set(file2_data.keys())
-        unique_ids_file2 = set(file2_data.keys()) - set(file1_data.keys())
-
-        if unique_ids_file1:
-            output.write("Scheduled IDs present only in File 1:\n")
-            for scheduled_id in unique_ids_file1:
-                output.write(f"{scheduled_id}: {file1_data[scheduled_id]}\n")
-            output.write("\n")
-
-        if unique_ids_file2:
-            output.write("Scheduled IDs present only in File 2:\n")
-            for scheduled_id in unique_ids_file2:
-                output.write(f"{scheduled_id}: {file2_data[scheduled_id]}\n")
+        else:
+            output.write("All scheduled IDs match between the two log files.\n")
 
 # Usage example:
 compare_log_files()
